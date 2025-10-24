9a834426d229096780c74a190717d8def3d582a6
Deepti Kottamasu <deeptikottamasu@Deeptis-MacBook-Air.local>
2025-10-24 18:18:45 +0530

    Adding Db access

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.routes.base import router as base_router
 import os
 from dotenv import load_dotenv
+from .models import Counter
+from .db import Base, engine, SessionLocal
 
 load_dotenv()
 
@@ -17,21 +20,32 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 # Include routers
 app.include_router(base_router, prefix="/api")
 
 counter = {"value": 0}
+
 
 @app.get("/api/health")
 async def health_check():
     return {"status": "ok", "message": "FastAPI is running!"}
 
 @app.post("/api/counter/increment")
-def increment_counter():
-    print("Incrementing...")
-    counter["value"] += 1
-    return {"count": counter["value"]}
+def increment_counter(db: Session = Depends(get_db)):
+    counter = db.query(Counter).first()
+    counter.value += 1
+    db.commit()
+    db.refresh(counter)
+    return {"count": counter.value}
 
 @app.get("/api/counter")
-async def get_counter():
-    return {"count": counter["value"]}
+def get_counter(db: Session = Depends(get_db)):
+    counter = db.query(Counter).first()
+    return {"count": counter.value}
